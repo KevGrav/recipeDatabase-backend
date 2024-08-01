2ddb421842f6921f2e9982582a9630af2f18e449
Kev Grav <kgravell.prsvr@gmail.com>
2024-08-01 19:59:30 -0400

    worked on get route

--- routes/recipes/controller/recipesController.js
+++ routes/recipes/controller/recipesController.js
@@ -1,26 +1,22 @@
 const bcrypt = require('bcryptjs')
 const Recipes = require('../model/Recipes')
+const APP_ID = "a889f78e"
+const APP_KEY = "3ffab496cec4b6ed3496b78b560d5950"
+import Axios from "axios"
+const searchString = require('..../recipeDatabase-frontend/')
 
-const searchRecipes = async (searchTerm, page)=> {
-    if(!apiKey) {
-        return error("API key note found")
-    }
-    const url = new URL("https://api.edamam.com/recipies/comlpexSearch") 
-    
-    const queryParams = {
-        apiKey,
-        query: searchTime,
-        number: "10",
-        offset: (page * 10).toString()
-    }
-    url.search = new URLSearchParams(queryParams).toString()
-    
+async function getRecipes(req, res){
     try {
-        const searchResponse = await fetch(url)
-        const resultsJson = await searchResponse.json()
-        return resultsJson
-    } catch (error) {
-        console.log(error)    
+        const foundRecipes = await Axios.get(
+            `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+            res.json({message: "Recipes Found", payload: foundRecipes})
+        } catch (error) {
+           res.status(500).json({message: "Error", error: error}) 
     }
 }
-module.exports = {}
+    
+     
+    
+    
+    
+module.exports = {getRecipes}
